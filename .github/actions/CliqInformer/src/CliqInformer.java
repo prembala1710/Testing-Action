3fc1b70f412cbd83a583dc921c91d181dedd7464
Prem Balasubramanian <83074315+prembala1710@users.noreply.github.com>
2023-03-09 10:58:11 +0530

    Update CliqInformer.java

--- .github/actions/CliqInformer/src/CliqInformer.java
+++ .github/actions/CliqInformer/src/CliqInformer.java
@@ -268,11 +268,11 @@ public class CliqInformer {
 						for (HashMap<String,String> PageDetails : PageArray)
 						{
 						    if(PageDetails.get("title").toLowerCase().contains("_footer"))
-							message = "\nThe (Footer)[" + PageDetails.get("html_url") + "] has been " + PageDetails.get("action");
+							message = message + "\\nThe (Footer)[" + PageDetails.get("html_url") + "] has been " + PageDetails.get("action");
 						    else if(PageDetails.get("title").toLowerCase().contains("_sidebar"))
-							message = "\nThe (Sidebar)[" + PageDetails.get("html_url") + "] has been " + PageDetails.get("action");
+							message = message + "\\nThe (Sidebar)[" + PageDetails.get("html_url") + "] has been " + PageDetails.get("action");
 						    else
-							message = "\nThe Page (" + PageDetails.get("html_url") + ") has been " + PageDetails.get("action") ;
+							message = message + "\\nThe Page (" + PageDetails.get("html_url") + ") has been " + PageDetails.get("action") ;
 						}
 						message = message + " \\n" + RepositoryURL + "/wiki/";
 					}
